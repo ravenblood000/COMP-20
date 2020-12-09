31c2acb89c06f3a98ec95f5640d314b9db142e2f
Kateri L <kateri.lalicker@tufts.edu>
2020-12-09 18:15:48 -0500

    Update stockFind.js

--- stockFind.js
+++ stockFind.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require('url');
 var port = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://23lalickerk:Bobbin101!@cluster0.cetrw.mongodb.net/HW12?retryWrites=true&w=majority";
+const MongoURL = "mongodb+srv://23lalickerk:Bobbin101!@cluster0.cetrw.mongodb.net/HW12?retryWrites=true&w=majority";
 
 http.createServer(function (req, res)
 {
@@ -10,25 +10,15 @@ http.createServer(function (req, res)
   var qobj = url.parse(req.url, true).query;
   var text = qobj.search;
   res.write("The search value is " + text);
-  MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+  MongoClient.connect(MongoURL, { useUnifiedTopology: true }, function(err, db)
+  {
     if(err) {console.log("Connection err: " + err); return;}
-  
-    var dbo = db.db("HW12");
-    var coll = dbo.collection("companies");
+      var dbo = db.db("HW12");
+      var coll = dbo.collection("companies");
 
-    theQuery = {$or {"company":"FLWS"},{"ticker":"FLWS"}}
-    coll.find(theQuery).toArray(function(err, items)
-    {
-      if (err) {console.log("Error: " + err);} 
-      else 
-      {
-	console.log("Items: ");
-	for (i=0; i<items.length; i++)
-	  console.log(i + ": " + items[i].title + " by: " + items[i].author);				
-      }   
-	  db.close();
-	});  //end find		
-});  //end connect
+      db.close();
+    });  //end find		
+  });  //end connect
 
 
 
