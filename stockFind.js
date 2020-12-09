9b236aec276c0927610c6a4f317e53ad00851547
Kateri L <kateri.lalicker@tufts.edu>
2020-12-09 18:55:25 -0500

    Update stockFind.js

--- stockFind.js
+++ stockFind.js
@@ -3,12 +3,13 @@ var url = require('url');
 var port = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient;
 const MongoURL = "mongodb+srv://23lalickerk:Bobbin101!@cluster0.cl1u9.mongodb.net/?retryWrites=true&w=majority";
+var text;
 
 http.createServer(function (req, res)
 {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var qobj = url.parse(req.url, true).query;
-  var text = qobj.search;
+  text = qobj.search;
   res.write("The search value is " + text);
   
   MongoClient.connect(MongoURL, { useUnifiedTopology: true }, function(err, db)
@@ -16,9 +17,9 @@ http.createServer(function (req, res)
     if(err) {console.log("Connection err: " + err); return;}
       var dbo = db.db("HW12");
       var coll = dbo.collection("companies");
-
+	console.log(text);
       db.close();		
   });  //end connect
-
+  // search after connection?
   res.end();
 }).listen(port);
