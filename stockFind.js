bd5d1f0b814092471eb08a8c19b97349cfa345b6
ravenblood000 <62115756+ravenblood000@users.noreply.github.com>
2020-12-09 04:11:30 -0500

    Add files via upload

--- stockFind.js
+++ stockFind.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var url = require('url');
+var port = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient;
 const mongoUrl = "mongodb+srv://23lalickerk:Bobbin101!@cluster0.cl1u9.mongodb.net/HW12?retryWrites=true&w=majority";
 
-MongoClient.connect(mongoUrl, {useUnifiedTopology: true}, function(err, db)
+/*MongoClient.connect(mongoUrl, {useUnifiedTopology: true}, function(err, db)
 {
   if(err){ console.log("Connection err: " + err); return; }
 
@@ -21,12 +22,14 @@ MongoClient.connect(mongoUrl, {useUnifiedTopology: true}, function(err, db)
     db.close();
   });  // end find		
 });  // end connect
+*/
 
-//http.createServer(function (req, res)
-//{
-  //res.writeHead(200, {'Content-Type': 'text/html'});
-  //var qobj = url.parse(req.url, true).query;
-  //var text = qobj.search;
-  //console.log("Search value: " + text);
-  //res.end();
-//}).listen(8080);
+http.createServer(function (req, res)
+{
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var qobj = url.parse(req.url, true).query;
+  var text = qobj.search;
+  res.write("<h2>Hello World!</h2>");
+  res.write("The search value is " + text);
+  res.end();
+}).listen(port);
