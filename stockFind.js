6c35db32a3c9075c288b3228d503e7bd5c221e73
ravenblood000 <62115756+ravenblood000@users.noreply.github.com>
2020-12-09 22:51:45 -0500

    Add files via upload

--- stockFind.js
+++ stockFind.js
@@ -1,37 +1,52 @@
 var http = require('http');
 var url = require('url');
-//var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient;
 const MongoURL = "mongodb+srv://23lalickerk:Bobbin101!@cluster0.cl1u9.mongodb.net/HW12?retryWrites=true&w=majority";
-var text = "";
-var results = "";
 
-/* 
 http.createServer(function (req, res)
 {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var qobj = url.parse(req.url, true).query;
-  text = qobj.search;
-*/  
-
-  MongoClient.connect(MongoURL, {useUnifiedTopology: true}, function(err, db)
+  var text = qobj.searchC;
+  var radio = qobj.search;
+  res.write("<h1>Database Results: </h1>");
+  var x = doit(text, radio);
+  x.then(function(s)
   {
-    if(err) {console.log("Connection err: " + err); return;}
-      var dbo = db.db("HW12");
-      var coll = dbo.collection("companies");
-      
-      theQuery = {$or: [{"company": text},{"ticker": text}]}
-      coll.find(theQuery).toArray(function(err, items)
-      {
-	if (err) {console.log("Error: " + err);} 
-	else 
-	{
-          console.log("Items: ");
-	  for (i = 0; i < items.length; i++)
-	    console.log(i + ": " + items[i].company + " by: " + items[i].ticker);				
-	}   
-      });
-      db.close();		
-  });  //end connect
-//  res.end("End");
-//}).listen(8080);
+    res.write(s);
+    res.end();
+  });      
+}).listen(port);
+
+async function doit(input, rad)
+{
+   client = new MongoClient(MongoURL, { useUnifiedTopology: true});
+   try
+   {
+     await client.connect();
+     var dbo = client.db("HW12");
+     var coll = dbo.collection("companies");
+     var results = "";
+     query = {}
+     if (rad == "ticker")
+       query = {"ticker": input}
+     else
+     {query = {"company": input}}
+
+     const curs = coll.find(query);
+     if ((await curs.count()) === 0)
+     {results = "No documents found!";}
+     await curs.forEach(function(item)
+     {
+       results += ("Company Name: " + item.company + ", Company Ticker: " + item.ticker + "<br>");
+     });
+   } 
+   catch(err)
+   {console.log("Database error: " + err);}
+   finally
+   {
+     client.close();
+     return results;
+   }
+} // end of doit()
