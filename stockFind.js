afa394a14119fafc3714bf66e7f411cfa52cc446
Kateri L <kateri.lalicker@tufts.edu>
2020-12-09 20:14:48 -0500

    Update stockFind.js

--- stockFind.js
+++ stockFind.js
@@ -2,16 +2,18 @@ var http = require('http');
 var url = require('url');
 //var port = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient;
-const MongoURL = "mongodb+srv://23lalickerk:Bobbin101!@cluster0.cl1u9.mongodb.net/?retryWrites=true&w=majority";
+const MongoURL = "mongodb+srv://23lalickerk:Bobbin101!@cluster0.cl1u9.mongodb.net/HW12?retryWrites=true&w=majority";
 var text = "";
 var results = "";
 
-/* http.createServer(function (req, res)
+/* 
+http.createServer(function (req, res)
 {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var qobj = url.parse(req.url, true).query;
   text = qobj.search;
 */  
+
   MongoClient.connect(MongoURL, {useUnifiedTopology: true}, function(err, db)
   {
     if(err) {console.log("Connection err: " + err); return;}
@@ -28,7 +30,7 @@ var results = "";
 	  for (i = 0; i < items.length; i++)
 	    console.log(i + ": " + items[i].company + " by: " + items[i].ticker);				
 	}   
-
+      });
       db.close();		
   });  //end connect
 //  res.end("End");
